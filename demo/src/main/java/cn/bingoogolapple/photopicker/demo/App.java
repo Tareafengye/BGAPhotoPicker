59b6bfcf031795b0921f796785eedbcf05208284
liu <18885640674@163.com>
2018-07-02 16:45:58 +0800

    第一次提交

---
+++ demo/src/main/java/cn/bingoogolapple/photopicker/demo/App.java
@@ -0,0 +1,18 @@
+package cn.bingoogolapple.photopicker.demo;
+
+import android.app.Application;
+
+
+/**
+ * 作者:王浩 邮件:bingoogolapple@gmail.com
+ * 创建时间:16/7/11 上午10:23
+ * 描述:
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        LeakCanary.install(this);
+    }
+}
